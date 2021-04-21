364bc01ac78aed7a537c7221fa8ce92af661ef27
Cheolkie Kim <ckiekim@naver.com>
2021-04-21 14:52:53 +0900

    Chapter 3

---
+++ ch03-2.R
@@ -0,0 +1,11 @@
+# 벡터(Vector, 1차원 데이터)
+s <- c(1,2,3,4,5,6)
+s2 <- c(1:6)       # 시작:끝
+s3 <- c(6:1)
+s4 <- 1:5
+c(1:3, c(4:6))
+c(1:30)
+seq(1, 100, by=2)   # from, to, increment
+seq(from=100, to=1, by=-3)
+seq(0, 1, by=0.1)
+seq(0, 1, length.out=11)
