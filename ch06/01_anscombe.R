6c9f216f9af4aa34d6cdeb589568a613644b8038
Cheolkie Kim <ckiekim@naver.com>
2021-04-28 10:22:25 +0900

    Anscombe

--- ch06/01_anscombe.R
+++ ch06/01_anscombe.R
@@ -22,6 +22,16 @@ summary(anscombe)
 # 그래프 그리기
 library(ggplot2)
 
+ggplot(anscombe) +
+    geom_point(aes(x1,y1),color='darkorange',size=3) +
+    scale_x_continuous(breaks=seq(2,20,2)) + 
+    scale_y_continuous(breaks=seq(2,14,2)) +
+    xlim(2,20) +
+    ylim(2,14) +
+    geom_abline(slope=0.5, intercept=3,
+                color='cornflowerblue',size=1) +
+    labs(title='Dataset 1')
+
 p1 <- ggplot(anscombe) +
     geom_point(aes(x1,y1),color='darkorange',size=3) +
     scale_x_continuous(breaks=seq(2,20,2)) + 
@@ -41,7 +51,6 @@ p2 <- ggplot(anscombe) +
     geom_abline(intercept=3, slope=0.5, 
                 color='cornflowerblue', size=1) +
     labs(title='Dataset II')
-p2
 p3 <- ggplot(anscombe) +
     geom_point(aes(x3,y3),color='darkorange',size=3) +
     scale_x_continuous(breaks=seq(2,20,2)) + 
@@ -51,7 +60,6 @@ p3 <- ggplot(anscombe) +
     geom_abline(intercept=3, slope=0.5, 
                 color='cornflowerblue', size=1) +
     labs(title='Dataset III')
-p3
 p4 <- ggplot(anscombe) +
     geom_point(aes(x4,y4),color='darkorange',size=3) +
     scale_x_continuous(breaks=seq(2,20,2)) + 
@@ -61,7 +69,35 @@ p4 <- ggplot(anscombe) +
     geom_abline(intercept=3, slope=0.5, 
                 color='cornflowerblue', size=1) +
     labs(title='Dataset IV')
-p4
 
 library(gridExtra)
 grid.arrange(p1,p2,p3,p4, ncol=2, top="Anscombe's Quartet")
+
+#######################################################################
+
+# Source Refactoring
+x <- ggplot(anscombe) +
+    geom_point(aes(x4,y4),color='darkorange',size=3)
+m1 <- x
+assign(paste('m',4,sep='.'), x)
+grid.arrange(p1,p2,p3,m.4, ncol=2, top="Anscombe's Quartet")
+
+library(dplyr)
+
+for (i in 1:4) {
+    tmp <- ggplot(anscombe) +
+        geom_point(aes(anscombe[,i], anscombe[,4+i]),
+                   color='darkorange',size=3) +
+        scale_x_continuous(breaks=seq(2,20,2)) + 
+        scale_y_continuous(breaks=seq(2,14,2)) +
+        xlim(2,20) +
+        ylim(2,14) +
+        geom_abline(intercept=3, slope=0.5, 
+                    color='cornflowerblue', size=1) +
+        labs(title=paste0('Dataset ',i),
+             x=paste0('x',i), y=paste0('y',i))
+    #tmp <- list(tmp)
+    assign(paste('m', i, sep='.'), tmp)
+    rm(tmp)
+}
+grid.arrange(m.1,m.2,m.3,m.4, ncol=2, top="Anscombe's Quartet")
