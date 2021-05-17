f8e619021e36d0da074d5888318f8e3f49eac52c
Cheolkie Kim <ckiekim@naver.com>
2021-05-17 14:23:38 +0900

    프로젝트

---
+++ project/4조.R
@@ -0,0 +1,18 @@
+library(leaflet)
+library(stringr)
+
+fest <- read.csv('project/대전축제.txt', sep='\t')
+head(fest)
+gu <- lapply(fest$소재지지번주소, function(x) unlist(str_split(x, ' '))[2])
+fest$gu <- unlist(gu)
+
+colors <- c('red','blue','green','purple','orange')
+pal <- colorFactor(colors, domain = fest$gu) 
+leaflet(fest) %>% 
+    setView(lng=127.39, lat=36.35, zoom=11) %>% 
+    addTiles() %>% 
+    addCircles(lng=~경도, lat=~위도, label=~축제명,
+               weight=1, fillColor=~pal(gu), radius=800,
+               fillOpacity=0.7) %>% 
+    addLegend(values = ~gu, position = 'bottomright', pal = pal)
+colorFactor(domain=fest$gu, fest$col)
